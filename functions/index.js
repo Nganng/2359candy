ce667a0ee67baba64521dc6b44a35cef3d56b75a
Tan Benjamin <>
2019-06-09 15:28:31 +0800

    add authentication to http endpoints. add refresh token endpoint

--- functions/index.js
+++ functions/index.js
@@ -4,20 +4,67 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
+const cookieParser = require('cookie-parser')();
 
 admin.initializeApp(functions.config().firebase);
-
 const db = admin.firestore();
-
 const app = express();
 const main = express();
 
 app.use(cors({ origin: true }));
+app.use(cookieParser);
 main.use('/api/v1', app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 
 exports.webApi = functions.https.onRequest(main);
+
+
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateUser = async (req, res, next) => {
+    console.log('Check if request is authorized with Firebase ID token');
+  
+    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+        !(req.cookies && req.cookies.__session)) {
+      console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+          'Make sure you authorize your request by providing the following HTTP header:',
+          'Authorization: Bearer <Firebase ID Token>',
+          'or by passing a "__session" cookie.');
+      res.status(403).send('Unauthorized');
+      return;
+    }
+  
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+      console.log('Found "Authorization" header');
+      // Read the ID Token from the Authorization header.
+      idToken = req.headers.authorization.split('Bearer ')[1];
+    } else if(req.cookies) {
+      console.log('Found "__session" cookie');
+      // Read the ID Token from cookie.
+      idToken = req.cookies.__session;
+    } else {
+      // No cookie
+      res.status(403).send('Unauthorized');
+      return;
+    }
+  
+    try {
+      const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+      console.log('ID Token correctly decoded', decodedIdToken);
+      req.user = decodedIdToken;
+      next();
+      return;
+    } catch (error) {
+      console.error('Error while verifying Firebase ID token:', error);
+      res.status(403).send('Unauthorized');
+      return;
+    }
+  };
 
 
 
@@ -83,7 +130,25 @@ app.post('/login', (req, res) => {
         })
     });
 })
-
+app.post('/refreshIdToken', (req, res) =>{
+    if(!req.body.refresh_token) {
+        res.status(400).send({
+            message: "refresh_token is required"
+        })
+    }
+    axios.post(`https://securetoken.googleapis.com/v1/token?key=${functions.config().project.webkey}`, {
+        grant_type: 'refresh_token',
+        refresh_token: req.body.refresh_token
+    })
+    .then((response) => {
+        res.status(200).send(response.data)
+    })
+    .catch((err) => {
+        res.status(400).send({
+            message: "An error has occured authenticating this user " + err.message
+        })
+    });
+})
 
 
 
@@ -115,7 +180,7 @@ app.post('/postings', (req, res) => {
     }
 })
 // Get all postings
-app.get('/postings', (req, res) => {
+app.get('/postings', validateUser, (req, res) => {
     db.collection('postings').get()
     .then(ref => {
         if (ref.empty) {
@@ -136,7 +201,7 @@ app.get('/postings', (req, res) => {
     });
 })
 // Get a posting
-app.get('/postings/:postingId', (req, res) => {
+app.get('/postings/:postingId', validateUser, (req, res) => {
     db.collection('postings').doc(req.params.postingId).get()
     .then(ref => {
         if (ref.data()) {
@@ -155,7 +220,7 @@ app.get('/postings/:postingId', (req, res) => {
     });
 })
 // Update a posting
-app.put('/postings/:postingId', (req, res) => {
+app.put('/postings/:postingId', validateUser, (req, res) => {
     const validationError = validatePostingInput(req.body);
     if (!validationError) {
         db.collection('postings').doc(req.params.postingId).update(req.body)
@@ -173,7 +238,7 @@ app.put('/postings/:postingId', (req, res) => {
     }
 })
 // Delete a posting 
-app.delete('/postings/:postingId', (req, res) => {
+app.delete('/postings/:postingId', validateUser, (req, res) => {
     db.collection('postings').doc(req.params.postingId).delete()
     .then(ref => {
         res.status(200).send(ref)
@@ -200,7 +265,7 @@ function validatePostingInput(body) {
 
 
 // Add new candidate
-app.post('/candidates', (req, res) => {
+app.post('/candidates', validateUser, (req, res) => {
     const validationError = validateCandidateInput(req.body);
     if (!validationError) {
         req.body.createdAt = admin.firestore.FieldValue.serverTimestamp()
@@ -220,7 +285,7 @@ app.post('/candidates', (req, res) => {
     }
 })
 // Get all candidates
-app.get('/candidates', (req, res) => {
+app.get('/candidates', validateUser, (req, res) => {
     db.collection('candidates').get()
     .then(ref => {
         if (ref.empty) {
@@ -241,7 +306,7 @@ app.get('/candidates', (req, res) => {
     });
 })
 // Get a candidate
-app.get('/candidates/:candidateId', (req, res) => {
+app.get('/candidates/:candidateId', validateUser, (req, res) => {
     db.collection('candidates').doc(req.params.candidateId).get()
     .then(ref => {
         if (ref.data()) {
@@ -260,7 +325,7 @@ app.get('/candidates/:candidateId', (req, res) => {
     });
 })
 // Update a candidate
-app.put('/candidates/:candidateId', (req, res) => {
+app.put('/candidates/:candidateId', validateUser, (req, res) => {
     const validationError = validateCandidateInput(req.body);
     if (!validationError) {
         db.collection('candidates').doc(req.params.candidateId).update(req.body)
@@ -278,7 +343,7 @@ app.put('/candidates/:candidateId', (req, res) => {
     }
 })
 // Delete a candidate 
-app.delete('/candidates/:candidateId', (req, res) => {
+app.delete('/candidates/:candidateId', validateUser, (req, res) => {
     db.collection('candidates').doc(req.params.candidateId).delete()
     .then(ref => {
         res.status(200).send(ref)
@@ -314,7 +379,7 @@ function validateCandidateInput(body) {
 
 
 // Add new note
-app.post('/candidates/:candidateId/notes', (req, res) => {
+app.post('/candidates/:candidateId/notes', validateUser, (req, res) => {
     db.collection('candidates').doc(req.params.candidateId).get()
     .then(doc => {
         if (!doc.exists) {
@@ -348,7 +413,7 @@ app.post('/candidates/:candidateId/notes', (req, res) => {
     });
 })
 // Get all notes of a candidate
-app.get('/candidates/:candidateId/notes', (req, res) => {
+app.get('/candidates/:candidateId/notes', validateUser, (req, res) => {
     db.collection('candidates').doc(req.params.candidateId)
     .collection('notes').get()
     .then(ref => {
@@ -370,7 +435,7 @@ app.get('/candidates/:candidateId/notes', (req, res) => {
     });
 })
 // Update a note
-app.put('/candidates/:candidateId/notes/:noteId', (req, res) => {
+app.put('/candidates/:candidateId/notes/:noteId', validateUser, (req, res) => {
     const validationError = validateNoteInput(req.body);
     if (!validationError) {
         db.collection('candidates').doc(req.params.candidateId)
@@ -389,7 +454,7 @@ app.put('/candidates/:candidateId/notes/:noteId', (req, res) => {
     }
 })
 // Delete a note 
-app.delete('/candidates/:candidateId/notes/:noteId', (req, res) => {
+app.delete('/candidates/:candidateId/notes/:noteId', validateUser, (req, res) => {
     db.collection('candidates').doc(req.params.candidateId).get()
     .then(doc => {
         if (!doc.exists) {
